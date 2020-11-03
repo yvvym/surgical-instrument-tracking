aa7c46987daa2c6f70fb0ee01650a3b73bc9ab65
sunsusan <susansun@sjtu.edu.cn>
2020-11-02 19:22:04 -0500

    need fram to video

---
+++ Archive/linedetection.m
@@ -0,0 +1,81 @@
+I = imread('target.png');
+[~,thre_img] = createMask_ycbcr(I);
+I = rgb2gray(thre_img);
+%I = I(:,1:1300);
+% I = imread('circuit.tif');
+figure;
+imshow(I)
+%rotI = imrotate(I,33,'crop');
+%figure;
+%imshow(rotI)
+rotI = I;
+BW = edge(rotI,'canny');
+figure;
+imshow(BW);
+% Compute the Hough transform of the binary image returned by edge.
+[H,theta,rho] = hough(BW);
+% Display the transform, H, returned by the hough function.
+figure
+imshow(imadjust(rescale(H)),[],...
+       'XData',theta,...
+       'YData',rho,...
+       'InitialMagnification','fit');
+xlabel('\theta (degrees)')
+ylabel('\rho')
+axis on
+axis normal 
+hold on
+colormap(gca,hot)
+% Find the peaks in the Hough transform matrix, H, using the houghpeaks function.
+P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
+% Superimpose a plot on the image of the transform that identifies the peaks.
+x = theta(P(:,2));
+y = rho(P(:,1));
+plot(x,y,'s','color','black');
+
+% Find lines in the image using the houghlines function.
+lines = houghlines(BW,theta,rho,P,'FillGap',5,'MinLength',5);
+% Create a plot that displays the original image with the lines superimposed on it.
+figure, imshow(rotI), hold on
+max_len = 0;
+
+  for k = 1:length(lines)
+    xy = [lines(k).point1; lines(k).point2];
+ %  xy = [219,949;1064,861];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+%    xy = [256,875;1064,861];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','cyan');
+% 
+%    % Plot beginnings and ends of lines
+%    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+% 
+%    xy = [182,1138;1035,909];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% 
+%    % Plot beginnings and ends of lines
+%    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+% 
+%    xy = [253,1061;1035,909];
+%    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','cyan');
+% 
+%    % Plot beginnings and ends of lines
+%    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+%    len = norm(lines(k).point1 - lines(k).point2);
+%    if (len > max_len)
+%       max_len = len;
+%       xy_long = xy;
+%    end
+  end
+% highlight the longest line segment
+% plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
+hold off
